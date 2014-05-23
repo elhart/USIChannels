a1947023a0dd7d9836a9afb4f492d40a6ad9ca7a
ivan.elhart <ivan.elhart@usi.ch>
2014-05-23 17:04:02 +0200

    - update "coordinates" post message support

--- app/controllers/USIChannel.java
+++ app/controllers/USIChannel.java
@@ -47,14 +47,15 @@ public class USIChannel extends Controller {
 	public static String appDisplayName = "USI Channels";
 	
 	// url base
-	//public static String urlBase = "uc-dev.inf.usi.ch";
-	public static String urlBase = "pdnet.inf.unisi.ch";
+	public static String urlBase = "uc-dev.inf.usi.ch";
+	//public static String urlBase = "pdnet.inf.unisi.ch";
 	
 	// wsAddress
 	public static String wsAddress = "ws://"+urlBase+":9015/usichannel/socket/";
 	
 	//scheduling domain (where the scheduler runs)
-	public static String schedulingDomain = "http://"+urlBase+":9009";
+	//public static String schedulingDomain = "http://"+urlBase+":9009";
+	public static String schedulingDomain = "http://"+"localhost"+":9009";
 	
 	// web app address
 	public static String webAppAddress = "http://"+urlBase+"/usiapps/";
